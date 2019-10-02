2958432ac81300f9c324e541cb03bec5e187623a
Nicolas Harlem Eide <nicolas@harlemeide.net>
2019-10-02 12:20:54 +0200

    Add information about myself

---
+++ nicolas/cv.py
@@ -0,0 +1,38 @@
+"""Nicolas Harlem Eide's CV in glorious python"""
+
+name = "Nicolas Harlem Eide"
+
+about = "Software developer with a passion for automation and simplicity.\n" \
+        "Trying to help people achieve what they want with less effort."
+
+experience = [
+    {"Employer": "mnemonic",
+     "Position": "Developer",
+     "Period": "2018 - present"},
+
+    {"Employer": "mnemonic",
+     "Position": "Security Analyst",
+     "Period": "2017 - 2018"},
+
+    {"Employer": "UiO",
+     "Position": "Student Recruiter",
+     "Period": "2016 - 2017"},
+]
+
+education = [
+    {"University": "UiO",
+     "Type": "M.Sc.",
+     "Name": "Programming and Networks",
+     "Period": "2018 - 2020"},
+
+    {"University": "UiO",
+     "Type": "B.Sc.",
+     "Name": "Programming and Networks",
+     "Period": "2015 - 2018"},
+]
+
+contact = {
+    "Phone": "+47 924 77 442",
+    "E-mail": "nicolas@harlemeide.net",
+}
+
---
+++ tests/test_cv.py
@@ -0,0 +1,24 @@
+from nicolas import cv
+
+
+def test_that_i_managed_to_spell_my_name_correctly():
+    assert cv.name == "Nicolas Harlem Eide"
+
+
+def test_that_i_say_something_about_myself():
+    assert len(cv.about) > 1
+
+
+def test_that_i_have_some_experience_with_programming_in_the_real_world():
+    assert len(cv.experience) > 1
+
+
+def test_that_i_actually_have_an_education():
+    """Did you know that I've got a bachelor and a master degree?"""
+    assert any("b.sc." in degree["type"] for degree in cv.education)
+    assert any("m.sc." in degree["type"] for degree in cv.education)
+
+
+def test_that_i_can_be_contacted_by_a_potential_employer():
+    assert "phone" in cv.contact
+    assert "e-mail" in cv.contact

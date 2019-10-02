e9dd9ac3244ba44dc879947fcf5ad0bd30cd565a
Nicolas Harlem Eide <nicolas@harlemeide.net>
2019-10-02 12:57:31 +0200

    Add a header formatter for outputting my name

--- nicolas/cv.py
+++ nicolas/cv.py
@@ -1,4 +1,5 @@
 """Nicolas Harlem Eide's CV in glorious python"""
+import shutil
 
 name = "Nicolas Harlem Eide"
 
@@ -35,4 +36,38 @@ contact = {
     "Phone": "+47 924 77 442",
     "E-mail": "nicolas@harlemeide.net",
 }
+
+
+def header(text: str, *,
+           lines: int = 7,
+           decorating_character: str = "="):
+    """Prints the text as a header
+
+    :param text: Text to display to the user
+    :param lines: Amount of lines that the header will occupy
+    :param decorating_character: The character that will be displayed around
+                                 the specified text.
+    :return: A string with the width of the users terminal with centered text.
+    """
+    line_length = shutil.get_terminal_size()[0]
+    text_line_decorator_length = (line_length - len(text) - 2) // 2
+
+    def centered_text(string):
+        centered = "".join([decorating_character * text_line_decorator_length,
+                            " " + string + " ",
+                            decorating_character * text_line_decorator_length])
+        centered += decorating_character * (line_length - len(centered))
+
+        return centered
+
+    output = []
+    for line in range(lines):
+        if line == lines // 2:
+            output.append(centered_text(text))
+        elif line == lines // 2 + 1 or line == lines // 2 - 1:
+            output.append(centered_text(" " * len(text)))
+        else:
+            output.append(decorating_character * line_length)
+
+    return "\n".join(output)
 
--- tests/test_cv.py
+++ tests/test_cv.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from nicolas import cv
 
 
@@ -22,3 +24,28 @@ def test_that_i_actually_have_an_education():
 def test_that_i_can_be_contacted_by_a_potential_employer():
     assert "phone" in cv.contact
     assert "e-mail" in cv.contact
+
+
+@mock.patch("shutil.get_terminal_size")
+def test_that_header_prints_full_terminal_width(terminal_size_mock):
+    expected_columns = 80
+    test_text = "Test"
+
+    terminal_size_mock.return_value = (expected_columns, 24)
+    output = cv.header(test_text)
+
+    assert test_text in output
+    assert all(len(line) is expected_columns for line in output.splitlines())
+
+
+@mock.patch("shutil.get_terminal_size")
+def test_that_header_prints_full_terminal_width_with_odd_input(
+        terminal_size_mock):
+    expected_columns = 80
+    test_text = "Testing"
+
+    terminal_size_mock.return_value = (expected_columns, 24)
+    output = cv.header(test_text)
+
+    assert test_text in output
+    assert all(len(line) is expected_columns for line in output.splitlines())
